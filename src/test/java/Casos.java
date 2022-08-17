85903a3eb47bcbef6ee2bd10f2911a79c2db673a
Traym17 <51390112+Traym17@users.noreply.github.com>
2022-08-16 18:10:56 -0600

    Update Casos.java

--- src/test/java/Casos.java
+++ src/test/java/Casos.java
@@ -1,2 +1,45 @@
 public class Casos {
+  Filtros filtros;
+
+    @Parameters({"navegador"})
+    @BeforeClass
+    public void beforeClass(String navegador) {
+        filtros = new Filtros();
+    }
+    @Parameters({"url"})
+    @BeforeMethod
+    public void beforeTest(String url) throws IOException {
+        filtros.abrirUrl(url);
+    }
+    @Test
+    public void PruebaSanJose() throws IOException {
+        filtros.dropSanjose();
+    }
+    @Test
+    public void PruebaAlajuela() throws IOException {
+        filtros.dropAlajueja();
+    }
+    @Test
+    public void PruebaCartago() throws IOException {
+        filtros.dropCartago();
+    }
+    @Test
+    public void PruebaGuanacaste() throws IOException {
+        filtros.dropGuanacaste();
+    }
+    @Test
+    public void PruebaPuntarenas() throws IOException {
+        filtros.dropPuntareanas();
+    }
+    @Test
+    public void PruebaLimon() throws IOException {
+        filtros.dropLimon();
+    }
+    @AfterMethod
+    public void afterTest() {
+    }
+    @AfterClass
+    public void afterClass() {
+        filtros.cerrarDriver();
+    }
 }
